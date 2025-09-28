ed9c6003c454a068c9f775dc733d0398cd6523bd
Jebreil Jhon Blancada <jebreilblancada20@gmail.com>
2025-09-28 18:33:55 +0800

    Working login tenant and admin with authentication using sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+Route::post('/login',[AuthController::class,'login']);
+
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::get('/tenant/dashboard',function (Request $request) {
+        if(!$request->user()->tokenCan('tenant')){
+                abort(403, 'Unauthorized');
+        }
+        return response()->json(['message'=>'Welcome Tenant']);
+    });
+    Route::get('/admin/dashboard',function (Request $request){
+        if(!$request->user()->tokenCan('admin')){
+            abort(403, 'Unauthorized    ');
+        }
+        return response()->json(['message'=> 'Welcome Admin!']);
+    });
+});
